ee385bc209b525ec2c7e048896a6641fc49f3d9a
TannabiZz <120448549+CannabiZz9@users.noreply.github.com>
2023-03-31 13:57:03 +0700

    Update sketch_mar01a.ino

--- sketch_mar01a.ino
+++ sketch_mar01a.ino
@@ -10,8 +10,9 @@ int LDR = A0;
 int LDRReading = 0;
 int led1 = 6;
 int led2 = 7;
-int buzzer = 8;
+int buzzer = 9;
 int buttonState = 0;
+int buttonState1 = 0;
 int emergencybuttonState = 0;
 int count_value = 0;
 int prestate = 0;
@@ -23,8 +24,7 @@ bool nightdoor = false;
 bool stopdoor = false;
 bool flooddor = false;
 bool isOpen = false;
-const int servoPin1 = 9;
-int speed = 5;
+const int servoPin1 = 8;
 VarSpeedServo myservo1;
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
@@ -50,7 +50,6 @@ void loop()
   updatelcd();
   waitforcom();
 
-
 }
 
 long microsecondsToCentimeters(long microseconds)
@@ -71,22 +70,25 @@ void check() {
   duration = pulseIn(inPin, HIGH);
 
   cm = microsecondsToCentimeters(duration);
-  if (cm < 10) {
+
+  if (cm <= 6) {
     outputstate += " flood! ";
     flooddor = true;
   }
-  else if (cm >= 10) {
+  else if (cm > 6) {
     flooddor = false;
   }
-  if (LDRReading > 300) {
+  if (LDRReading < 300) {
     outputstate += " Night! ";
     nightdoor = true;
     daydoor = false;
+    digitalWrite(led1, HIGH);
   }
   else {
     outputstate += " Day! ";
     daydoor = true;
     nightdoor = false;
+    digitalWrite(led1, LOW);
   }
 }
 
@@ -101,54 +103,236 @@ void updatelcd() {
 
 void waitforcom() {
   buttonState = digitalRead(buttonPin1);
-
+  buttonState1 = digitalRead(buttonPin2);
   if (buttonState == HIGH && prestate == 0) {
     count_value++;
+    Serial.println("Button press");
+    Serial.println(isOpen);
+    checkenvi();
     delay(100);
     prestate = 1;
   } else if (buttonState == LOW) {
     prestate = 0;
   }
-  if (count_value % 2 == 1) {
-    checkenvi();
-  }
-  else if (count_value % 2 == 0) {
-    checkenvi();
-  }
-
-  if (flooddor) {
+  if (flooddor && !isOpen) {
     opendoor50();
-    //led buzzer
+    digitalWrite(led2, HIGH);
+    delay(50);
+    digitalWrite(led2, LOW);
+    delay(50);
+    digitalWrite(led2, HIGH);
+    buzz();
+    digitalWrite(led2, LOW);
+    delay(50);
+    digitalWrite(led2, HIGH);
     isOpen = true;
+  }
+  else if (!flooddor && isOpen) {
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led2, LOW);
+  }
+  else if (!flooddor && !isOpen) {
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led2, LOW);
   }
 }
 
 
 void checkenvi() {
-
   if (isOpen) {
     closedoor();
     isOpen = false;
+    Serial.println("closedoor");
   }
   else if (!isOpen && daydoor && !nightdoor) {
     opendoor100();
     isOpen = true;
+    Serial.println("opendoor100");
   }
   else if (!isOpen && !daydoor && nightdoor) {
     opendoor50();
-    //led
     isOpen = true;
+    Serial.println("opendoor50");
   }
 }
 
 void opendoor50() {
-  //servohere
+  myservo1.write(180, 3); 
+  delay(1600); 
+  myservo1.write(90, 5); 
+  delay(600);
 }
 
 void opendoor100() {
-  //servohere
+  myservo1.write(180, 3); 
+  delay(1300); 
+  myservo1.write(90, 5); 
+  delay(600);
+  myservo1.write(180, 3); 
+  delay(1300); 
+  myservo1.write(90, 5); 
+  delay(600);
 }
 
 void closedoor() {
-  //servohere
+  myservo1.write(0, 3);
+  delay(1750);
+  myservo1.write(90, 5);
+  delay(1000);
+}
+
+
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST      0
+
+
+// change this to make the song slower or faster
+int tempo = 140;
+
+
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+int melody[] = {
+
+  // Happy Birthday
+  // Score available at https://musescore.com/user/8221/scores/26906
+
+  NOTE_C4, 4, NOTE_C4, 8,
+  NOTE_D4, -4, NOTE_C4, -4, NOTE_F4, -4,
+  NOTE_E4, -2, NOTE_C4, 4, NOTE_C4, 8,
+  NOTE_D4, -4, NOTE_C4, -4, NOTE_G4, -4,
+  NOTE_F4, -2, NOTE_C4, 4, NOTE_C4, 8,
+
+  NOTE_C5, -4, NOTE_A4, -4, NOTE_F4, -4,
+  NOTE_E4, -4, NOTE_D4, -4, NOTE_AS4, 4, NOTE_AS4, 8,
+  NOTE_A4, -4, NOTE_F4, -4, NOTE_G4, -4,
+  NOTE_F4, -2,
+
+};
+
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+// this calculates the duration of a whole note in ms
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+
+void buzz() {
+  // iterate over the notes of the melody.
+  // Remember, the array is twice the number of notes (notes + durations)
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+
+    // calculates the duration of each note
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 0.5; // increases the duration in half for dotted notes
+    }
+
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+
+    // Wait for the specief duration before playing the next note.
+    delay(noteDuration);
+
+    // stop the waveform generation before the next note.
+    noTone(buzzer);
+  }
 }
